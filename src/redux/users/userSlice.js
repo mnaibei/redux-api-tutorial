2971e62ab4ef231878b7143cdcb28ee19b4bb444
mnaibei <muchajulius@gmail.com>
2023-05-09 15:10:27 +0300

    redux with api

---
+++ src/redux/users/userSlice.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk, isRejectedWithValue } from '@reduxjs/toolkit';
+
+const url = 'https://randomuser.me/api/?results=5';
+
+const initialState = {
+  users: [],
+  isLoading: true,
+  error: undefined,
+};
+
+export const getUsers = createAsyncThunk('users/getUsers', async () => {
+  try {
+    const result = await fetch(url);
+    const data = await result.json();
+    return data.results;
+  } catch (error) {
+    return isRejectedWithValue(error.message);
+  }
+});
+
+const userSlice = createSlice({
+  name: 'users',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUsers.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getUsers.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.users = action.payload;
+        state.error = null;
+      })
+      .addCase(getUsers.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default userSlice.reducer;
